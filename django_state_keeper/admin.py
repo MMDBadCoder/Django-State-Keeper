a95c0b7b01b13f5b9fa2d43209e16e81eaffead7
mohammad <gitmmdgit@gmail.com>
2023-09-30 00:49:40 +0330

    add exception for not adding urls

--- django_state_keeper/admin.py
+++ django_state_keeper/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-
+from django.urls.exceptions import NoReverseMatch
 from django_state_keeper.auto_backup_model import AutoBackupService
 from django_state_keeper.loading_backup_model import BackupUpload
 from django_state_keeper.packaging_backup_model import BackupPackaging
@@ -20,7 +20,13 @@ class BackupPackagingAdmin(admin.ModelAdmin):
     ordering = ['id']
 
     def download_button(self, movie):
-        url = reverse('admin-download-backup', args=[movie.id])
+        try:
+            url = reverse('admin-download-backup', args=[movie.id])
+        except NoReverseMatch:
+            raise Exception("You should include django-state-keeper.urls to your project urls!"
+                            "\nAdd something like this in your urls.py file:"
+                            "\n\"urlpatterns += [path('your-customized-path/', include('django_state_keeper.urls'))]\"")
+
         button_html = f'<a class="button" href="{url}" target="_blank">Download</a>'
         return mark_safe(button_html)
 
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-state-keeper',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description="A Django app that allows you to create backup of project and restore it later.",
+    long_description=open('README.md').read(),
+    url='https://github.com/MMDBadCoder/Django-State-Keeper',
+    author='Mohammad Heydari',
+    author_email='heidary13794@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'Django>=3.2',
+        'python-telegram-bot>=20.5'
+    ],
+)
